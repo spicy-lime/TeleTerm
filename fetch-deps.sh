f9ba63a4e47c3a0ddc89a4e0ca81be8fd7a3feef
none <none>
2025-03-29 23:08:46 -0400

    added sources to jediterm

--- fetch-deps.sh
+++ fetch-deps.sh
@@ -3,8 +3,12 @@ set -e
 git clone https://github.com/spicy-lime/jediterm ext/
 pushd ext
 gradle distZip
+gradle sourceJar :core:sourcesJar
+gradle sourceJar :ui:sourcesJar
 mv ./.gradleBuild/JediTerm/distributions/JediTerm-3.51-SNAPSHOT.zip .
 unzip JediTerm-3.51-SNAPSHOT.zip
+mv ./.gradleBuild/ui/libs/jediterm-ui-3.51-SNAPSHOT-sources.jar JediTerm-3.51-SNAPSHOT/lib/
+mv ./.gradleBuild/core/libs/jediterm-core-3.51-SNAPSHOT-sources.jar JediTerm-3.51-SNAPSHOT/lib/
 mkdir unpacked
 pushd unpacked
 for f in ../JediTerm-3.51-SNAPSHOT/lib/*.jar; do
